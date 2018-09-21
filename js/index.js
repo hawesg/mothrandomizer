dd7e4c53d7c57e765ac70f0721d82066e0ebb46e
Garrett Hawes <ghawes@uci.edu>
2018-09-21 01:51:08 -0700

    Update 2
    Fixed images, fixed formatting, added dice icon, added settings.

--- js/index.js
+++ js/index.js
@@ -1,142 +1,158 @@
+
 new Vue({
   el: '#app',
-  data: function data() {return {
+  data: {
       firstTime: true,
       isActive:false,
       dialog: false,
-      participants: [
-      {
+      pickDelay: 2500,
+      participants:[],
+      fullTime: [
+    {
         id: 1,
         name: 'Garrett Hawes',
         picked: false,
-        image: "http://garretthawes.com/moth/i/garrett.jpg" },
-
-      {
+        image: "images/garrett.jpg" },
+    {
         id: 2,
         name: 'Hannes Rusterholz',
         picked: false,
-        image: "http://garretthawes.com/moth/i/hannes.jpg" },
+        image: "images/hannes.jpg" },
 
-      {
+    {
         id: 3,
         name: 'Jon Deirmenjian',
         picked: false,
-        image: "http://garretthawes.com/moth/i/jon.jpg" },
+        image: "images/jon.jpg" },
 
-      {
+    {
         id: 4,
         name: 'Natasha Patel',
         picked: false,
-        image: "http://garretthawes.com/moth/i/natasha.jpg" },
+        image: "images/natasha.jpg" },
 
-      {
+    {
         id: 5,
         name: 'Ricky Lobel',
         picked: false,
-        image: "http://garretthawes.com/moth/i/ricky.jpg" },
+        image: "images/ricky.jpg" },
 
-      {
+    {
         id: 6,
         name: 'Ranjith Kankanala',
         picked: false,
-        image: "http://garretthawes.com/moth/i/ranjith.jpg" },
+        image: "images/ranjith.jpg" },
 
-      {
+    {
         id: 7,
         name: 'Zayed Buali',
         picked: false,
-        image: "http://garretthawes.com/moth/i/male.jpg" },
+        image: "images/zayed.jpg" },
 
-      {
+    {
         id: 8,
         name: 'Jeremy Ross',
         picked: false,
-        image: "http://garretthawes.com/moth/i/jeremy.jpg" },
+        image: "images/jeremy.jpg" },
 
-      {
+    {
         id: 9,
         name: 'Jacqueline Jelloian',
         picked: false,
-        image: "http://garretthawes.com/moth/i/jacqueline.jpg" },
+        image: "images/jacqueline.jpg" },
 
-      {
+    {
         id: 10,
         name: 'Pandu Setiawan',
         picked: false,
-        image: "http://garretthawes.com/moth/i/male.jpg" },
+        image: "images/pandu.jpg" },
 
-      {
+    {
         id: 11,
         name: 'Alex Chikovani',
         picked: false,
-        image: "http://garretthawes.com/moth/i/alex.jpg" },
+        image: "images/alex.jpg" },
 
-      {
+    {
         id: 12,
         name: 'Peter Dickinson',
         picked: false,
-        image: "http://garretthawes.com/moth/i/peter.jpg" },
+        image: "images/peter.jpg" },
 
-      {
+    {
         id: 13,
         name: 'Saloni Vyas',
         picked: false,
-        image: "http://garretthawes.com/moth/i/female.jpg" },
+        image: "images/saloni.jpg" },
 
-      {
+    {
         id: 14,
         name: 'Melanie Kane',
-        image: "http://garretthawes.com/moth/i/melanie.jpg" },
+        image: "images/melanie.jpg" },
 
-      {
+    {
         id: 15,
         name: 'Ken Yeh',
         picked: false,
-        image: "http://garretthawes.com/moth/i/ken.jpg" },
+        image: "images/ken.jpg" },
 
-      {
+    {
         id: 16,
         name: 'Hendrino Usnal',
         picked: false,
-        image: "http://garretthawes.com/moth/i/hendrino.jpg" }
-        ],
+        image: "images/hendrino.jpg" },
+        {id: 17,
+         name: 'Ensieh Bahrami',
+         picked: false,
+         image: "images/ensieh.jpg" }
+],
+      currentClass: 0,
+      classes: ["Full Time", "Professional"],
+      e1: "Full Time",
       pool: [],
       picked: [],
-      roll: {name: 'Loading...', image: "http://garretthawes.com/moth/i/roll.gif"},
-      current: {}
-  };
+      roll: {name: '', image: "images/roll.gif"},
+      current: {
+          image: "images/init.jpg"
+      }
   },
-
-
-  created: function created() {
-    // `this` points to the vm instance
-    this.$data.pool = _.cloneDeep(this.$data.participants);
+   computed: {
+        listIsEmpty: function () {
+            return _.size(this.picked)==0;
+        }
+    },
+  created: function () {
+    //this.participants = _.cloneDeep(fullTime);
+    this.participants = _.cloneDeep(this.fullTime);
+    this.pool = _.cloneDeep(this.participants);
     this.$data.picked = [];
   },
   methods: {
-    pick: function pick() {
-      if(this.$data.firstTime) {
-          this.$data.firstTime = false;
+    pick: function () {
+      if(this.firstTime) {
+          this.firstTime = false;
           this.$vuetify.goTo("#picker", {
               duration: 800,
               offset: 0,
               easing: 'easeInOutCubic'
           });
       }
-      this.$data.current = this.$data.roll;
-      var y = _.sample(this.$data.pool);
-      _.remove(this.$data.pool, function (n) {
+      this.current = this.roll;
+      var y = _.sample(this.pool);
+      _.remove(this.pool, function (n) {
         return n.id == y.id;
       });
-      this.$data.isActive=true;
+      this.isActive=true;
       setTimeout(() => {
-        this.$data.current = y;
-        this.$data.picked = _.concat(y, this.$data.picked);
-        this.$data.isActive=false;
-      }, 2000);
+        this.current = y;
+        this.picked = _.concat(y, this.$data.picked);
+        this.isActive=false;
+      }, this.pickDelay);
     },
     clear: function clear() {
-      this.$data.pool = _.cloneDeep(this.$data.participants);
-      this.$data.picked = [];
-      this.$data.current = [];
+      this.pool = _.cloneDeep(this.participants);
+      this.picked = [];
+      this.current = {
+          image: "images/init.jpg"
+      };
     } } });
