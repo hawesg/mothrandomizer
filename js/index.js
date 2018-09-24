3752171638fb6949aed2228d9443f41df428bf5c
Garrett Hawes <ghawes@uci.edu>
2018-09-24 02:08:50 -0700

    graphics, settings, time settup
    Change graphics, add the rest of section 2, fix settings, setup auto pick for time

--- js/index.js
+++ js/index.js
@@ -1,130 +1,157 @@
 
 var vu = new Vue({
-  el: '#app',
-  data: {
-      firstTime: true,
-      isActive:false,
-      dialog: false,
-      pickDelay: 2500,
-      participants:[],
-      section1: [
-    {
-        name: 'Garrett Hawes',
-        image: "images/section1/garrett.jpg" },
-    {
-        name: 'Hannes Rusterholz',
-        image: "images/section1/hannes.jpg" },
+    el: '#app',
+    data: {
+        fab: false,
+        isActive:false,
+        dialog: false,
+        pickDelay: 2500,
+        participants:[],
+        section1: [
+            {
+                name: 'Garrett Hawes',
+                image: "images/section1/garrett.jpg" },
+            {
+                name: 'Hannes Rusterholz',
+                image: "images/section1/hannes.jpg" },
 
-    {
-        name: 'Jon Deirmenjian',
-        image: "images/section1/jon.jpg" },
+            {
+                name: 'Jon Deirmenjian',
+                image: "images/section1/jon.jpg" },
 
-    {
-        name: 'Natasha Patel',
-        image: "images/section1/natasha.jpg" },
+            {
+                name: 'Natasha Patel',
+                image: "images/section1/natasha.jpg" },
 
-    {
-        name: 'Ricky Lobel',
-        image: "images/section1/ricky.jpg" },
+            {
+                name: 'Ricky Lobel',
+                image: "images/section1/ricky.jpg" },
 
-    {
-        name: 'Ranjith Kankanala',
-        image: "images/section1/ranjith.jpg" },
+            {
+                name: 'Ranjith Kankanala',
+                image: "images/section1/ranjith.jpg" },
 
-    {
-        name: 'Zayed Buali',
-        image: "images/section1/zayed.jpg" },
+            {
+                name: 'Zayed Buali',
+                image: "images/section1/zayed.jpg" },
 
-    {
-        name: 'Jeremy Ross',
-        image: "images/section1/jeremy.jpg" },
+            {
+                name: 'Jeremy Ross',
+                image: "images/section1/jeremy.jpg" },
 
-    {
-        name: 'Jacqueline Jelloian',
-        image: "images/section1/jacqueline.jpg" },
+            {
+                name: 'Jacqueline Jelloian',
+                image: "images/section1/jacqueline.jpg" },
 
-    {
-        name: 'Pandu Setiawan',
-        image: "images/section1/pandu.jpg" },
+            {
+                name: 'Pandu Setiawan',
+                image: "images/section1/pandu.jpg" },
 
-    {
-        name: 'Alex Chikovani',
-        image: "images/section1/alex.jpg" },
+            {
+                name: 'Alex Chikovani',
+                image: "images/section1/alex.jpg" },
 
-    {
-        name: 'Peter Dickinson',
-        image: "images/section1/peter.jpg" },
+            {
+                name: 'Peter Dickinson',
+                image: "images/section1/peter.jpg" },
 
-    {
-        name: 'Saloni Vyas',
-        image: "images/section1/saloni.jpg" },
+            {
+                name: 'Saloni Vyas',
+                image: "images/section1/saloni.jpg" },
 
-    {
-        name: 'Melanie Kane',
-        image: "images/section1/melanie.jpg" },
+            {
+                name: 'Melanie Kane',
+                image: "images/section1/melanie.jpg" },
 
-    {
-        name: 'Ken Yeh',
-        image: "images/section1/ken.jpg" },
+            {
+                name: 'Ken Yeh',
+                image: "images/section1/ken.jpg" },
 
-    {
-        name: 'Hendrino Usnal',
-        image: "images/section1/hendrino.jpg" },
-        {
-         name: 'Ensieh Bahrami',
-         image: "images/section1/ensieh.jpg" }
-],
-      section2: [
-          {
-              name: 'Angelica',
-              image: "images/section2/angelica.jpg" },
-          {
-              name: 'Atorina',
-              image: "images/section2/atorina.jpg" },
-          {
-              name: 'Brian',
-              image: "images/section2/brian.jpg" },
-          {
-              name: 'Hang',
-              image: "images/section2/hang.jpg" },
-          {
-              name: 'Hrag',
-              image: "images/section2/hrag.jpg" },
-          {
-              name: 'Liz',
-              image: "images/section2/liz.jpg" },
-          {
-              name: 'Maria',
-              image: "images/section2/maria.jpg" },
-          {
-              name: 'McKenzie',
-              image: "images/section2/mckenzie.jpg" },
-          {
-              name: 'Ricky',
-              image: "images/section2/ricky.jpg" },
-          {
-              name: 'Tai',
-              image: "images/section2/tai.jpg" },
-          {
-              name: 'Theresa Keh',
-              image: "images/section2/theresa.jpg" },
-          {
-              name: 'Turki',
-              image: "images/section2/turki.jpg" },
-          {
-              name: 'Vipul Vatsyayan',
-              image: "images/section2/vipul.jpg" }
-      ],
-      currentClass: [],
-      classes: ["Section 1", "Section 2"],
-      currentSection: "Section 1",
-      pool: [],
-      picked: [],
-      roll: {name: '', image: "images/section1/roll.gif"},
-      current: {
-          image: "images/init.jpg"
-      }
-  },
+            {
+                name: 'Hendrino Usnal',
+                image: "images/section1/hendrino.jpg" },
+            {
+                name: 'Ensieh Bahrami',
+                image: "images/section1/ensieh.jpg" }
+        ],
+        section2: [
+            {
+                name: 'Angelica',
+                image: "images/section2/angelica.jpg" },
+            {
+                name: 'Atorina',
+                image: "images/section2/atorina.jpg" },
+            {
+                name: 'Brian',
+                image: "images/section2/brian.jpg" },
+            {
+                name: 'Hang',
+                image: "images/section2/hang.jpg" },
+            {
+                name: 'Hrag',
+                image: "images/section2/hrag.jpg" },
+            {
+                name: 'Liz',
+                image: "images/section2/liz.jpg" },
+            {
+                name: 'Maria',
+                image: "images/section2/maria.jpg" },
+            {
+                name: 'McKenzie',
+                image: "images/section2/mckenzie.jpg" },
+            {
+                name: 'Ricky',
+                image: "images/section2/ricky.jpg" },
+            {
+                name: 'Tai',
+                image: "images/section2/tai.jpg" },
+            {
+                name: 'Theresa Keh',
+                image: "images/section2/theresa.jpg" },
+            {
+                name: 'Turki',
+                image: "images/section2/turki.jpg" },
+            {
+                name: 'Vipul Vatsyayan',
+                image: "images/section2/vipul.jpg" },
+            {
+                name: 'Aulton',
+                image: "images/section2/aulton.jpg" },
+            {
+                name: 'Bahruz',
+                image: "images/section2/bahruz.jpg" },
+            {
+                name: 'Carlos',
+                image: "images/section2/carlos.jpg" },
+            {
+                name: 'Curtis',
+                image: "images/section2/curtis.jpg" },
+            {
+                name: 'Derek',
+                image: "images/section2/derek.jpg" },
+            {
+                name: 'Eric',
+                image: "images/section2/eric.jpg" },
+            {
+                name: 'Adrineh',
+                image: "images/section2/adrineh.jpg" },
+            {
+                name: 'Zach',
+                image: "images/section2/zach.jpg" },
+            {
+                name: 'Jenna',
+                image: "images/section2/jenna.jpg" }
+        ],
+        currentClass: [],
+        classes: ["Section 1", "Section 2"],
+        currentSection: "",
+        pool: [],
+        picked: [],
+        roll: {name: '', image: "images/section1/roll.gif"},
+        current: {
+            image: "images/init2.jpg"
+        }
+    },
     watch: {
         currentSection: function (val) {
             this.participants = (val == "Section 1") ? this.section1 : this.section2;
@@ -132,52 +159,46 @@ var vu = new Vue({
             this.pool = _.cloneDeep(this.participants);
             this.picked = [];
             this.current = {
-                image: "images/init.jpg"
+                image: "images/init2.jpg"
             };
         }
     },
-   computed: {
-        listIsEmpty: function () {
+    computed: {
+        pickedIsEmpty: function () {
             return _.size(this.picked)==0;
+        },
+        poolIsEmpty: function(){
+            return _.size(this.participants)==_.size(this.picked);
         }
     },
-  created: function () {
-    this.participants = _.cloneDeep(this.section1);
-    this.pool = _.cloneDeep(this.participants);
-    this.$data.picked = [];
-  },
-  methods: {
-    pick: function () {
-      if(this.firstTime) {
-          this.firstTime = false;
-          this.$vuetify.goTo("#picker", {
-              duration: 800,
-              offset: 0,
-              easing: 'easeInOutCubic'
-          });
-      }
-      if(_.size(this.pool)!=0){
-          this.current = this.roll;
-          var y = _.sample(this.pool);
-          _.remove(this.pool, function (n) {
-              return n.name == y.name;
-          });
-          this.isActive=true;
-          setTimeout(() => {
-              this.current = y;
-              this.picked = _.concat(y, this.$data.picked);
-              this.isActive=false;
-          }, this.pickDelay);
-      }
-      else{
-
-      }
+    created: function () {
+        var date = new Date();
+        this.currentSection=(date.getHours()==23||(date.getHours()==13&&date.getMinutes()<20))?"Section 2":"Section 1";
     },
-    clear: function clear() {
-      this.pool = _.cloneDeep(this.participants);
-      this.picked = [];
-      this.current = {
-          name: "",
-          image: "images/init.jpg"
-      };
-    } } });
+    methods: {
+        pick: function () {
+            if(_.size(this.pool)!=0){
+                this.current = this.roll;
+                var y = _.sample(this.pool);
+                _.remove(this.pool, function (n) {
+                    return n.name == y.name;
+                });
+                this.isActive=true;
+                setTimeout(() => {
+                    this.current = y;
+                    this.picked = _.concat(y, this.$data.picked);
+                    this.isActive=false;
+                }, this.pickDelay);
+            }
+            else{
+
+            }
+        },
+        clear: function clear() {
+            this.pool = _.cloneDeep(this.participants);
+            this.picked = [];
+            this.current = {
+                name: "",
+                image: "images/init2.jpg"
+            };
+        } } });
