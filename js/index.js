c3c4145029471e402350da007e11563e6b33489e
Garrett Hawes <ghawes@uci.edu>
2018-10-29 01:12:14 -0700

    Update js/index.js
    Remove Hannes

--- js/index.js
+++ js/index.js
@@ -13,9 +13,6 @@ var vu = new Vue({
             {
                 name: 'Garrett Hawes',
                 image: "images/sm/Section1/garrett.jpg" },
-            {
-                name: 'Hannes Rusterholz',
-                image: "images/sm/Section1/hannes.jpg" },
 
             {
                 name: 'Jon Deirmenjian',
